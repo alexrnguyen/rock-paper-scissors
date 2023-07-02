3a1ba75230ec1a0f5191b2b56d6a36eea410dbe6
alexrnguyen <alexander.nguyen@shaw.ca>
2023-07-02 13:25:47 -0600

    Create functions in script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+function getComputerChoice() {
+
+}
+
+function getPlayerChoice() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
